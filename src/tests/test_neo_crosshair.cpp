1dd5032992a8d0579fb26a7bfab78cfd8057f662
Rain <Rainyan@users.noreply.github.com>
2026-09-11 23:30:07 +0300

    wip

--- src/tests/test_neo_crosshair.cpp
+++ src/tests/test_neo_crosshair.cpp
@@ -334,7 +334,7 @@ void TestDeserial_V7_ALPHA_V35()
 	TEST_COMPARE_STR(szExportSeq, SERIAL_TEST_LATEST_STR);
 	TEST_COMPARE_INT(true, ValidateCrosshairSerial(szExportSeq));
 
-	ExportCrosshair(&xhairInfo, szExportSeq, NEOXHAIR_SERIAL_ALPHA_V29);
+	ExportCrosshair(&xhairInfo, szExportSeq, NEOXHAIR_SERIAL_ALPHA_V35);
 	TEST_COMPARE_STR(szExportSeq, SERIAL_TEST_STR);
 	TEST_COMPARE_INT(true, ValidateCrosshairSerial(szExportSeq));
 }
