19821ffa5f2d95eeb4efe100489437329d490a9d
Rain <Rainyan@users.noreply.github.com>
2026-09-11 22:55:37 +0300

    wip

--- src/tests/test_neo_crosshair.cpp
+++ src/tests/test_neo_crosshair.cpp
@@ -969,12 +969,12 @@ void TestFeature_Flags_OldBool_SepDotColor()
 
 void TestFeature_Flags_OldBool_ToplineOff_SepDotColor()
 {
-	// Convert v4 (non-compressed, 2 bools) to v6+ (flagged)
+	// Convert v4 (non-compressed, 2 bools) to v7+ (flagged)
 	static const char SERIAL_TEST_STR[] = "4;2;-1;0;3;0.000;2;4;1;6;0;5;7;1;1;-16776961;-16711936;-65536;";
 	static const char SERIAL_TEST_LATEST_STR[] = CURRENT_VER ",0,0,3,2,-1,0,3,2,4,1,6,5,7,1,-16776961,-16711936,-65536,";
 
 	CrosshairInfo xhairInfo = {};
-	const bool bValid = ImportCrosshair(&xhairInfo, SERIAL_TEST_STR);
+	const bool bValid = ImportCrosshair(&xhairInfo, SERIAL_TEST_STR, nullptr, NEOXHAIR_SERIAL_ALPHA_V22);
 	TEST_COMPARE_INT(bValid, true);
 	TEST_COMPARE_INT(xhairInfo.wepFlags, CROSSHAIR_WEP_FLAG_DEFAULT);
 	TEST_COMPARE_INT(xhairInfo.hipfireFlags, CROSSHAIR_HIPFIRECUSTOM_FLAG_NIL);
